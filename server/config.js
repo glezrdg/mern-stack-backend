bf24ce64566b105f4bf3ac80ae540ce454ad9799
German G <glezrdg@gmail.com>
2023-03-11 11:12:07 -0400

    sin git

--- server/config.js
+++ server/config.js
@@ -8,4 +8,3 @@ export const PORT = process.env.PORT || 4000;
 export const CLOUD_NAME = process.env.CLOUD_NAME;
 export const API_KEY = process.env.API_KEY;
 export const API_SECRET = process.env.API_SECRET;
-git 
